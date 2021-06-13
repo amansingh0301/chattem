5249cd2a4f0706c52a0a537add73f9ff56b7934f
amansingh0301 <as9997450@gmail.com>
2021-06-14 01:08:04 +0530

    added signin page with facebook and google

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,4 +1,6 @@
 import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/database';
 
 const config = {
     apiKey: "AIzaSyDD9c8W_ukadI7TO05uzrY1gz8CvVdpVOg",
@@ -6,8 +8,11 @@ const config = {
     projectId: "chat-web-app-ff759",
     storageBucket: "chat-web-app-ff759.appspot.com",
     messagingSenderId: "782356133864",
-    appId: "1:782356133864:web:ef558906739cea4b16965d"
+    appId: "1:782356133864:web:ef558906739cea4b16965d",
+    databaseURL:' https://chat-web-app-ff759-default-rtdb.europe-west1.firebasedatabase.app'
   };
 
   // eslint-disable-next-line no-unused-vars
   const app = firebase.initializeApp(config);
+  export const auth = app.auth();
+  export const database = app.database();
