028d3bc32c48a192bd6d4ddedec142427ff8131e
amansingh0301 <as9997450@gmail.com>
2021-06-23 21:44:44 +0530

    update all user referrence

--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -5,17 +5,21 @@ import EditableInput from '../EditableInput';
 import { database } from '../../misc/firebase';
 import ProviderBlock from './ProviderBlock';
 import AvatarUploadBtn from './AvatarUploadBtn';
+import { getUserUpdates } from '../../misc/helpers';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
 
   const onSave = async newData => {
-    const userNicknameRef = database
-      .ref(`/profiles/${profile.uid}`)
-      .child('name');
-
     try {
-      await userNicknameRef.set(newData);
+      const updates = await getUserUpdates(
+        profile.uid,
+        'name',
+        newData,
+        database
+      );
+
+      await database.ref().update(updates);
 
       Alert.success('Nickname has been updated', 4000);
     } catch (err) {
@@ -39,9 +43,8 @@ const Dashboard = ({ onSignOut }) => {
           onSave={onSave}
           label={<h6 className="mb-2">Nickname</h6>}
         />
-        <AvatarUploadBtn/>
+        <AvatarUploadBtn />
       </Drawer.Body>
-      
 
       <Drawer.Footer>
         <Button block color="red" onClick={onSignOut}>
