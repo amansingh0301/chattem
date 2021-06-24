4d844543952b1c3d053dc7ad224de99c6e0a6868
amansingh0301 <as9997450@gmail.com>
2021-06-24 21:52:56 +0530

    displayed audio messages and deleted from storage

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-param-reassign */
+/* eslint-disable consistent-return */
 import React, { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router';
 import { Alert } from 'rsuite';
-import { database, auth } from '../../../misc/firebase';
+import { database, auth, storage } from '../../../misc/firebase';
 import { transformToArrWithId } from '../../../misc/helpers';
 import MessageItem from './MessageItem';
 
@@ -86,7 +87,7 @@ const Messages = () => {
   }, []);
 
   const handleDelete = useCallback(
-    async msgId => {
+    async (msgId, file) => {
       // eslint-disable-next-line no-alert
       if (!window.confirm('Delete this message?')) {
         return;
@@ -114,7 +115,16 @@ const Messages = () => {
 
         Alert.info('Message has been deleted');
       } catch (err) {
-        Alert.error(err.message);
+        return Alert.error(err.message);
+      }
+
+      if (file) {
+        try {
+          const fileRef = storage.refFromURL(file.url);
+          await fileRef.delete();
+        } catch (err) {
+          Alert.error(err.message);
+        }
       }
     },
     [chatId, messages]
